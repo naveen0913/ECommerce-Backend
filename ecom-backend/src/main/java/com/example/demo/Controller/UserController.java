bb9313b459fef2507c6ace22047b7592d7b6e022
naveen0913 <udayagirinaveen135@gmail.com>
2025-03-25 23:17:02 +0530

    Added Cross-Origin

--- ecom-backend/src/main/java/com/example/demo/Controller/UserController.java
+++ ecom-backend/src/main/java/com/example/demo/Controller/UserController.java
@@ -8,13 +8,11 @@ import com.example.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
--- ecom-backend/src/main/java/com/example/demo/Controller/WishlistController.java
+++ ecom-backend/src/main/java/com/example/demo/Controller/WishlistController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/wishlist")
+@CrossOrigin("*")
 public class WishlistController {
     @Autowired
     WishlistService wishlistService;
